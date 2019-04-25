a12b1e2058397527e0e597f77956456626cb0f88
liubaoqiang1202 <liubaoqiang1202@163.com>
2019-04-25 22:22:26 +0800

    test

--- firstfile/helloword.py
+++ firstfile/helloword.py
@@ -1,1 +1,10 @@
-print("helloword")
+class Person:
+    def __init__(self,personName):
+        self.name = personName
+    def printName(self):
+        print("我的名字是：", self.name)
+def userInput(name):
+    userInputname = Person(name) #实例化对象
+    userInputname.printName() #调用函数
+
+userInput("liubaoqiang")
